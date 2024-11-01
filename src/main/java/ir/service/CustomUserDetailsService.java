3631ced74d44bad3cefcbd7b97826604e43484c3
Messbah.A <Messbah.A@Gmail.com>
2024-11-01 14:22:36 +0330

    DB Security Added

--- src/main/java/ir/service/CustomUserDetailsService.java
+++ src/main/java/ir/service/CustomUserDetailsService.java
@@ -1,13 +1,19 @@
 package ir.service;
 
+import ir.model.entity.Role;
 import ir.model.entity.User;
 import ir.repository.UserRepository;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,16 +26,19 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println("Load By User Name Security");
-        User user= userRepository.findByUsername(username);
+        User user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException(username);
-        }else{
-            return org.springframework.security.core.userdetails.User
-                    .builder()
-                    .username(user.getUsername())
-                    .password(user.getPassword())
-                    .authorities(new SimpleGrantedAuthority ("ADMIN"))
-                    .build();
+        } else {
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    mapRolesToAuthorities(user.getRoleSet()));
         }
     }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roleSet) {
+        return roleSet.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
 }
+
